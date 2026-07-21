c139d91df334f4765a13bb4d7f0cece9cfbd55a2
kamilnotaris-png <kamil.notaris@gmail.com>
2026-07-21 20:53:03 +0800

    Rapikan sitemap sesuai sinyal Google

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,15 +2,14 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ route('peta.index') }}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
+        @if ($lastModified)
+        <lastmod>{{ $lastModified->toAtomString() }}</lastmod>
+        @endif
     </url>
 @foreach ($titikWisata as $titik)
     <url>
         <loc>{{ route('titik-wisata.show', $titik) }}</loc>
         <lastmod>{{ $titik->updated_at->toAtomString() }}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
     </url>
 @endforeach
 </urlset>
